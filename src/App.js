aa24691d87efcbb119bbb5af9e5b34e1cc646f5e
Your Name <you@example.com>
2020-08-05 23:00:04 +0430

    some changes

--- src/App.js
+++ src/App.js
@@ -48,7 +48,7 @@ class App extends Component {
             <div className="App">
                 <h1>Hi, I'm a React App!</h1>
                 <p>This is really working!</p>
-                <button onClick={this.switchNameHandler.bind(this, 'Maximilian')}>Switch Name</button>                <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+                <button style={style} onClick={this.switchNameHandler.bind(this, 'Maximilian')}>Switch Name</button>                <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
                 <Person name={this.state.persons[1].name}
                         age={this.state.persons[1].age}
                         click={this.switchNameHandler.bind(this, 'Max!')}
